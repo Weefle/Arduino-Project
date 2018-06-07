7cf3a9f311936b91cd42bce14c8dd447e79eb309
Weefle <mr.weefle@gmail.com>
2018-06-07 11:25:28 +0200

    big update

--- arduino-project.ino
+++ arduino-project.ino
@@ -1,57 +1,79 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS A2
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature sensors(&oneWire);
+int ILS = 3;       
+int EtatILS = 0;
+float RayonDesBras = 0.07;   
+float Coef_Etalonnage = 20; 
+float VitesseVent = 0;
+long chrono = 0;              
+long chrono_depart = 0;      
+long duree_mesure = 2000;     
+volatile long compteur = 0;
 const int pinTemp = A0;
 const int B = 3975;
+int sens = 0;
 #define HALL_SENSOR_1 2
 #define HALL_SENSOR_2 7
 #define HALL_SENSOR_3 8
 void setup() {
   Serial.begin(9600);
+  sensors.begin();
   pinMode(2, INPUT);
   pinMode(8, INPUT);
   pinMode(7, INPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
+  pinMode(ILS, INPUT);
+  attachInterrupt(1, impulsions, RISING);
 
 }
-
+void impulsions() 
+{
+  compteur = compteur + 1 ;
+}
 void loop() {
-  // if(used==false){
-  float tempe = getTemp();
+  sensors.requestTemperatures();
+  float tempe = sensors.getTempCByIndex(0);
+  float temp = getTemp();
   if(isMagnet(HALL_SENSOR_1)){
-    if(tempe>40){
-      startRotation();
+    if(temp>34 && tempe>34){
+      startRotation(1);
     }else{
       stopRotation();
     }
   }else if(isMagnet(HALL_SENSOR_2)){
-    if(15<=tempe<25){
-      startRotation();
+    if(temp>34 && tempe>34){
+      startRotation(1);
+    }else if(temp<32 && tempe<32){
+      startRotation(0);
     }else{
       stopRotation();
     }
     }else if (isMagnet(HALL_SENSOR_3)){
-      if(tempe<15){
-      startRotation();
+      if(temp<32 && tempe<32){
+      startRotation(0);
     }else{
       stopRotation();
     }
     }else{
       stopRotation();
     }
+    Serial.println(tempe);
+    Serial.println(temp);
   }
-     /*if(tempe>40){
-      }
-  else if(15<=tempe<25){
-    
-  }
-  else if(tempe<15){
-    }*/
-  //Serial.println(tempe);
 
-void startRotation(){
+void startRotation(int sens){
   //mettre code pour améliorer avancement d'un coté ou autre avec boolean
-  digitalWrite(4, HIGH);
+  if(sens == 0){
+    digitalWrite(4, LOW);
+digitalWrite(5, HIGH);
+  }else if(sens == 1){
+    digitalWrite(4, HIGH);
 digitalWrite(5, LOW);
-//else LOW HIGH
+  }
 }
 
 void stopRotation(){
@@ -62,10 +84,6 @@ digitalWrite(5, LOW);
     digitalWrite(4, LOW);
 digitalWrite(5, LOW);
   }
-  /*digitalWrite(4, LOW);
-digitalWrite(5, LOW);
-Serial.println("Arret");*/
-//delay(time);
 }
 float getTemp(){
   int val = analogRead(pinTemp);
@@ -77,8 +95,35 @@ float getTemp(){
     float temperature = 1/(log(resistance/10000)/B+1/298.15)-273.15;
     return temperature;
     // Print the temperature to the serial console.
-   /* Serial.println(temperature); 
-    delay(1000);*/
+}
+float getVent(){
+  //lecture du capteur
+    EtatILS = digitalRead(ILS);
+    
+    if(EtatILS == HIGH)
+    {
+    digitalWrite(5, HIGH);      // Allume la Led
+    }
+    else 
+    {
+    digitalWrite(5, LOW);      // Eteind la Led
+    }  
+     
+  chrono = millis();
+  
+  if ((chrono - chrono_depart) >= duree_mesure)     // Test pour voir si temps du chrono >= durée de mesure
+  { 
+    VitesseVent = (2 * 3.14 * RayonDesBras * Coef_Etalonnage) * ((compteur/2) / (duree_mesure/1000))/3.6 ;   // compteur/2 (car 2 ILS/tour) et durée/1000 (car duree est en ms)
+                                                                                                             // division par 3,6 pour passer des m/sec en km/h
+    chrono_depart = millis();                      // remet le chrono de depart à 0
+    compteur = 0;
+  }
+        
+    return VitesseVent;
+}
+int getLux(){
+  int valeur = analogRead(A4);
+  return valeur/10;
 }
 boolean isMagnet(uint8_t magnet)
 {
